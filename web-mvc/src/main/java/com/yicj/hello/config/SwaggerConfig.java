09d421d954c6e07f8f24a6793ba6159b24cf512a
yichengjie <626659321@qq.com>
2023-11-26 20:10:31 +0800

    暂时保存

--- web-mvc/src/main/java/com/yicj/hello/config/SwaggerConfig.java
+++ web-mvc/src/main/java/com/yicj/hello/config/SwaggerConfig.java
@@ -1,47 +1,47 @@
-package com.yicj.hello.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
-
-import java.util.Collections;
-
-/**
- * @author lyr
- */
-
-@Configuration
-@EnableSwagger2
-@EnableOpenApi
-public class SwaggerConfig {
-
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.yicj.hello.controller"))
-//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("springboot mybatis plus框架demo")
-                .description("springboot mybatis plus框架demo")
-                .contact(new Contact("nelson", "https://www.aiprose.com/", "mail_yanpeng@163.com"))
-                .version("1.0")
-                .build();
-    }
-
-}
+//package com.yicj.hello.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.oas.annotations.EnableOpenApi;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+//
+//import java.util.Collections;
+//
+///**
+// * @author lyr
+// */
+//
+//@Configuration
+//@EnableSwagger2
+//@EnableOpenApi
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.yicj.hello.controller"))
+////                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("springboot mybatis plus框架demo")
+//                .description("springboot mybatis plus框架demo")
+//                .contact(new Contact("nelson", "https://www.aiprose.com/", "mail_yanpeng@163.com"))
+//                .version("1.0")
+//                .build();
+//    }
+//
+//}
